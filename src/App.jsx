91feaf5cccc36f0250faa087fedef7d53c4257ad
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-12-19 00:04:41 +0300

    Add BackgroundCanvas component and update Hero styles for improved visuals

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Hero, Navbar, Works, StarsCanvas } from "./components";
 import TechStack from "./components/TechStack";
+import BackgroundCanvas from "./components/canvas/Background";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
+        <BackgroundCanvas />
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
